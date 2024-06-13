e10ce6a79a4538f7306cfdb3094fe4d3e1828ee0
tomjoy75 <tomjoy75@gmail.com>
2024-06-13 17:41:47 +0200

    few modifications

--- doc/doc.md
+++ doc/doc.md
@@ -1,6 +1,11 @@
 ## SUJETS
 [Github](https://github.com/42-AI/bootcamp_python#module00---basics-1---eleven-commandments)
 
+## PYTHON
+[Style](https://peps.python.org/pep-0008/) \
+[zeste de savoir](https://zestedesavoir.com/tutoriels/2514/un-zeste-de-python/) \
+[doc python](https://docs.python.org/3/)
+
 ## NUMPY
 [Numpy Array](https://numpy.org/doc/stable/reference/routines.array-creation.html)
 
---
+++ module02/ex00/ft_map.py
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_map.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tjoyeux <tjoyeux@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/06/13 16:59:44 by tjoyeux           #+#    #+#              #
+#    Updated: 2024/06/13 17:11:38 by tjoyeux          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_map(function_to_apply, iterable):
+	"""Map the function to all elements of the iterable.
+	Args:
+	function_to_apply: a function taking an iterable.
+	iterable: an iterable object (list, tuple, iterator).
+	Return:
+	An iterable.
+	None if the iterable can not be used by the function.
+	"""
+	
